f5e44cfb9d37e5e344f0e03a5f5fe57bd0e4a80e
Nash E. Foster <leaf@pyrofex.net>
2019-11-19 10:45:17 -0700

    Remove built files.

--- third-party/check-0.12.0/check_stdint.h
+++
@@ -1,9 +0,0 @@
-#ifndef _CHECK_CHECK_STDINT_H
-#define _CHECK_CHECK_STDINT_H 1
-#ifndef _GENERATED_STDINT_H
-#define _GENERATED_STDINT_H "check 0.12.0"
-/* generated using gnu compiler gcc (Ubuntu 7.4.0-1ubuntu1~18.04.1) 7.4.0 */
-#define _STDINT_HAVE_STDINT_H 1
-#include <stdint.h>
-#endif
-#endif
