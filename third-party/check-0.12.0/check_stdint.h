db9738efd86fc3a980959fd27a79e33e39c09849
Mike Stay <stay@pyrofex.net>
2019-11-19 10:18:03 -0700

    Update check

--- third-party/check-0.12.0/check_stdint.h
+++ third-party/check-0.12.0/check_stdint.h
@@ -2,7 +2,7 @@
 #define _CHECK_CHECK_STDINT_H 1
 #ifndef _GENERATED_STDINT_H
 #define _GENERATED_STDINT_H "check 0.12.0"
-/* generated using gnu compiler gcc (Ubuntu 8.3.0-6ubuntu1) 8.3.0 */
+/* generated using gnu compiler gcc (Ubuntu 7.4.0-1ubuntu1~18.04.1) 7.4.0 */
 #define _STDINT_HAVE_STDINT_H 1
 #include <stdint.h>
 #endif
