4761f76b00498b5d983b5cc8c7285eb70d65194d
Nash E. Foster <leaf@pyrofex.net>
2020-01-18 16:03:04 -0700

    Filter third-party code from clang-format.

--- cmake/Modules/clang-format.cmake
+++ cmake/Modules/clang-format.cmake
@@ -10,5 +10,15 @@
 
 # get all project files
 file(GLOB_RECURSE ALL_SOURCE_FILES *.cpp *.h)
+
+# filter stuff in third-party
+set(PROJECT_TRDPARTY_DIR "third-party")
+foreach (SOURCE_FILE ${ALL_SOURCE_FILES})
+    string(FIND ${SOURCE_FILE} ${PROJECT_TRDPARTY_DIR} PROJECT_TRDPARTY_DIR_FOUND)
+    if (NOT ${PROJECT_TRDPARTY_DIR_FOUND} EQUAL -1)
+        list(REMOVE_ITEM ALL_SOURCE_FILES ${SOURCE_FILE})
+    endif ()
+endforeach ()
+
 add_custom_target(clangformat COMMAND /usr/bin/clang-format -style=Google
         -i ${ALL_SOURCE_FILES})
