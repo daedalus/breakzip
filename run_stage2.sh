70e0acf038eaf3e21299ec002acc625d721b258d
Nash E. Foster <leaf@pyrofex.net>
2020-02-04 15:56:58 -0700

    Fix a typo in run_stage2.sh

--- run_stage2.sh
+++ run_stage2.sh
@@ -8,11 +8,11 @@ SHARD_END=1000
 
 seq ${SHARD_BEGIN} ${SHARD_END} | xargs -I % mkdir -p stage2/% ; 
 seq ${SHARD_BEGIN} ${SHARD_END} | xargs -I % -P ${NUM_CORES} \
-    build.out/src/mitm_stage2/mitm_stage2 \
-        -target ../nullified.zip \
-        -input_shard stage1/nullified.% \
-        -output stage2/%/nullified \
-        >>stage2/${i}/stage2.log 2>&1 ;
+    sh -c "build.out/src/mitm_stage2/mitm_stage2 \
+           -target ../nullified.zip \
+           -input_shard stage1/nullified.% \
+           -output stage2/%/nullified \
+           >>stage2/%/stage2.log 2>&1";
 
 echo "Processed shards ${SHARD_BEGIN} through ${SHARD_END}"
 
