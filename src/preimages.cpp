176c0b4be41cd45102f7d61522bef10000aa1334
Nash E. Foster <leaf@pyrofex.net>
2020-02-04 19:38:06 -0700

    Add a program to output the static preimages table.

---
+++ src/preimages.cpp
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "breakzip.h"
+#include "breakzip_config.h"
+#include "crc32.h"
+#include "mitm_common.h"
+
+DECLARE_string(target);
+DECLARE_bool(runtests);
+DEFINE_string(input_shard, "target.out.0",
+              "The filename of the stage1 shard to run on.");
+DECLARE_string(output);
+DECLARE_int32(srand_seed);
+DEFINE_int32(stop_after, -1,
+             "If set to a positive value, the program "
+             "will stop after processing <stop_after> stage1 candidates.");
+
+using namespace mitm;
+using namespace std;
+using namespace breakzip;
+using namespace google;
+
+const char* usage_message = R"usage(
+    Usage: mitm_stage2 <FILE> <OUT>
+    Runs the stage2 attack using the stage1 data in FILE, the shard specified
+    by -shard, and writes output to the filename specified by -outfile with the
+    shard number appended.
+
+    If you pass the -runtests argument, then the tests will fail unless the
+    correct guess is contained within the shard you have provided via -target.
+    Stage1 prints the name of the shard containing the correct guess.
+    )usage";
+
+int main(int argc, char* argv[]) {
+    int my_argc = argc;
+
+    SetVersionString(version_string());
+    SetUsageMessage(usage_message);
+    auto non_flag = ParseCommandLineFlags(&my_argc, &argv, false);
+
+    // We build the preimages once for all candidates.
+    vector<vector<uint16_t>> preimages(0x100);
+    build_preimages(preimages);
+
+    printf("const uint16_t[256][64] = {\n");
+    for (int i = 0; i < preimages.size(); ++i) {
+        printf("    {");
+        for (int j = 0; j < preimages[i].size(); ++j) {
+            printf("%d, ", preimages[i][j]);
+        }
+        printf("},\n");
+    }
+    printf("};\n");
+
+    return 0;
+}
