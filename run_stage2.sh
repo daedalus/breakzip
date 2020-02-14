6a1562691be3b6578d49c899ad2da1b5e7a98c41
Nash E. Foster <leaf@pyrofex.net>
2020-02-14 23:08:00 +0000

    Rejigger scripts.

--- run_stage2.sh
+++ run_stage2.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 
-mkdir -p stage1
-build.out/src/mitm_stage1/mitm_stage1 -target ../nullified.zip -output stage1/nullified -shard_size 1000
-
-NUM_CORES=2
+NUM_CORES=96
 SHARD_BEGIN=0
-SHARD_END=21
+SHARD_END=10030
 
 seq ${SHARD_BEGIN} ${SHARD_END} | xargs -I % mkdir -p stage2/% ; 
 seq ${SHARD_BEGIN} ${SHARD_END} | xargs -t -I % -P ${NUM_CORES} sh -c "build.out/src/mitm_stage2/mitm_stage2 -target ../nullified.zip -input_shard stage1/nullified.% -output stage2/%/nullified >stage2/%/stage2.log 2>&1;"
