1bbbe5795ab21bc9df1d43b7e164062d769a71f3
Nash E. Foster <leaf@pyrofex.net>
2020-02-15 01:44:10 +0000

    Tweaks.

--- run_stage2.sh
+++ run_stage2.sh
@@ -4,8 +4,9 @@ NUM_CORES=96
 SHARD_BEGIN=0
 SHARD_END=10030
 
-seq ${SHARD_BEGIN} ${SHARD_END} | xargs -I % mkdir -p stage2/% ; 
-seq ${SHARD_BEGIN} ${SHARD_END} | xargs -t -I % -P ${NUM_CORES} sh -c "build.out/src/mitm_stage2/mitm_stage2 -target ../nullified.zip -input_shard stage1/nullified.% -output stage2/%/nullified >stage2/%/stage2.log 2>&1;"
+mkdir -p stage2
+touch stage2/start_time
+seq ${SHARD_BEGIN} ${SHARD_END} | xargs -t -I % -P ${NUM_CORES} sh -c "mkdir -p stage2/%; build.out/src/mitm_stage2/mitm_stage2 -target ../nullified.zip -input_shard stage1/nullified.% -output stage2/%/nullified >stage2/%/stage2.log 2>&1;"
 
 echo "Processed shards ${SHARD_BEGIN} through ${SHARD_END}"
 
